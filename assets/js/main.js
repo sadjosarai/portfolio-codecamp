382742a821b3f711aa87a02f09d291c66382dc33
sarai sadjo legbane <sarai.sadjo@facsciences-uy1.cm>
2023-11-20 19:00:47 +0100

    feat : qualification section added

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,9 +33,43 @@ navLink.forEach(n => n.addEventListener('click', linkAction))
 
 
 //====================== accordion skills
+const skillsContent =document.getElementsByClassName('skills__content'),
+      skillsHeader = document.querySelectorAll('.skills__header')
 
+function toggleSkills(){
+    let itemClass = this.parentNode.className
+
+    for(i = 0; i < skillsContent.length; i++){
+        skillsContent[i].className = 'skills__content skills__close'
+    }
+    if(itemClass === 'skills__content skills__close'){
+        this.parentNode.className =  'skills__content skills__open'
+    }
+}
+
+skillsHeader.forEach( (el) =>{
+    el.addEventListener('click', toggleSkills)
+})
 
 //======================= qualification tabs
+const tabs = document.querySelectorAll('[data-target]'),
+      tabContents = document.querySelectorAll('[data-content]')
+
+tabs.forEach(tab =>{
+    tab.addEventListener('click', ()=>{
+        const target = document.querySelector(tab.dataset.target)
+
+        tabContents.forEach(tabContent =>{
+            tabContent.classList.remove('qualification__active')
+        })
+        target.classList.add('qualification__active')
+
+        tab.forEach(tab =>{
+            tab.classList.remove('qualification__active')
+        })
+        tab.classList.add('qualification__active')
+    })
+})
 
 //================= services modal
 
@@ -45,4 +79,4 @@ navLink.forEach(n => n.addEventListener('click', linkAction))
 
 // ================== scroll section active link
 
-// ================= change background header
+// ================= change background header 
